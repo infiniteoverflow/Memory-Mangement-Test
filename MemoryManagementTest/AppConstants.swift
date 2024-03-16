4c5707ae8b1cf2ed3ac188b7596190f8f92e7922
Aswin Gopinathan <aswingopinathan1871@gmail.com>
2024-03-16 10:45:42 +0530

    Added HomeViewController

---
+++ MemoryManagementTest/AppConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppConstants.swift
+//  MemoryManagementTest
+//
+//  Created by Aswin Gopinathan on 16/03/24.
+//
+
+import Foundation
+import UIKit
+
+struct AppConstants {
+    static let lilac = UIColor(red: 190/255, green: 140/255, blue: 242/255, alpha: 1)
+    static let purple = UIColor(red: 90/255, green: 37/255, blue: 140/255, alpha: 1)
+}
---
+++ MemoryManagementTest/HomeViewController.swift
@@ -0,0 +1,86 @@
+//
+//  HomeViewController.swift
+//  MemoryManagementTest
+//
+//  Created by Aswin Gopinathan on 16/03/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: UI Properties
+    lazy var button = UIButton()
+
+    // MARK: VC Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = AppConstants.lilac
+        setupButton()
+    }
+    
+    // MARK: UI Methods
+    private func setupButton() {
+        view.addSubview(button)
+        button.setTitle("Go to next screen", for: .normal)
+        button.backgroundColor = AppConstants.purple
+        button.layer.cornerRadius = 24
+        button.addTarget(self, action: #selector(onButtonTap), for: .touchDown)
+        addConstraintForButton()
+    }
+    
+    private func addConstraintForButton() {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let centerXConstraint = NSLayoutConstraint(item: button,
+                                                   attribute: .centerX,
+                                                   relatedBy: .equal,
+                                                   toItem: view,
+                                                   attribute: .centerX,
+                                                   multiplier: 1,
+                                                   constant: 0)
+        let centerYConstraint = NSLayoutConstraint(item: button,
+                                                   attribute: .centerY,
+                                                   relatedBy: .equal,
+                                                   toItem: view,
+                                                   attribute: .centerY,
+                                                   multiplier: 1,
+                                                   constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: button,
+                                                  attribute: .height,
+                                                  relatedBy: .equal,
+                                                  toItem: nil,
+                                                  attribute: .notAnAttribute,
+                                                  multiplier: 1,
+                                                  constant: 54)
+        let widthConstraint = NSLayoutConstraint(item: button,
+                                                 attribute: .width,
+                                                 relatedBy: .equal,
+                                                 toItem: nil,
+                                                 attribute: .notAnAttribute,
+                                                 multiplier: 1,
+                                                 constant: 220)
+        
+        view.addConstraints([
+            centerXConstraint,
+            centerYConstraint,
+            heightConstraint,
+            widthConstraint
+        ])
+    }
+    
+    // MARK: Logic methods
+    @objc
+    private func onButtonTap() {
+        guard let navigationController = self.navigationController else {
+            return
+        }
+        navigationController.pushViewController(HomeViewController(), animated: true)
+    }
+}
+
+#Preview("HomeViewController", 
+         traits: .defaultLayout,
+         body: {
+    HomeViewController()
+})
