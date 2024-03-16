e4b60bfa8d6625e76ebe42ef4d3a6f32c770264b
Aswin Gopinathan <aswingopinathan1871@gmail.com>
2024-03-16 10:07:51 +0530

    Initial commit

---
+++ MemoryManagementTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryManagementTest
+//
+//  Created by Aswin Gopinathan on 16/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
